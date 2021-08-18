e9329f057b8241f11b858712a941f1a87bc5d0a7
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-18 17:03:08 +0100

    Update setup-python.sh

--- setup-python.sh
+++ setup-python.sh
@@ -8,33 +8,84 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
-# Optionally remove existing pyenv
+# Define function to set up pyenv
+function install_pyenv () {
+    echo "Installing pyenv to $HOME/.pyenv..."
+            
+    curl https://pyenv.run | bash
+    
+    echo 
+    echo "# Pyenv setup, generated automatically by Theo's setup-python.sh on $(date)" >> $HOME/.bashrc
+    echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
+    echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+    echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+    
+    echo "Done."
+}
+
+
+# Install pyenv, optionally removing existing pyenv
+echo 
+echo "Checking for pyenv install..."
+if [[ -d $HOME/.pyenv ]]
+then
+    while true
+    do
+        read -rp "Existing pyenv found at $HOME/.pyenv, overwrite it? [y/n]: " input
+        case $input in
+            [yY])
+                echo "Removing $HOME/.pyenv..."  
+                rm -rf $HOME/.pyenv
+                echo "Done."
+                install_pyenv
+                break
+                ;;
+            [nN])
+                echo "Skipping pyenv installation..."
+                echo "Done."
+                break
+                ;;
+            *)
+                echo "Please enter y or n."
+                ;;
+        esac
+    done
+else
+    echo "No existing pyenv found at $HOME/.pyenv."
+    install_pyenv
+fi
+
+# Install Python
+echo
 while true
 do
-    read -rp "Existing pyenv found at $HOME/.pyenv, overwrite it? [y/n]: " input
-    case $input in
+    read -rp "Install Python $DEFAULT_PYTHON_VERSION? [y/n]: " input1
+    case $input1 in
         [yY])
-            echo "Removing $HOME/.pyenv..."  
-            rm -rf $HOME/.pyenv
+            echo "Installing Python $DEFAULT_PYTHON_VERSION..."
+            pyenv install $DEFAULT_PYTHON_VERSION
             echo "Done."
-       
-            # Install pyenv
-            echo "Installing pyenv to $HOME/.pyenv..."
             
-            curl https://pyenv.run | bash
-
-            echo 
-            echo "# Pyenv setup, generated automatically by Theo's setup-python.sh on $(date)" >> $HOME/.bashrc
-            echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
-            echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
-            echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
-            
-            echo "Done."
-            break
+            while true
+            do
+                read -rp "Set Python $DEFAULT_PYTHON_VERSION as default local Python version? [y/n]: " input2
+                case $input2 in
+                    [yY])              
+                        echo "Setting local Python version to be $DEFAULT_PYTHON_VERSION..."
+                        pyenv local $DEFAULT_PYTHON_VERSION
+                        echo "Done."
+                        break
+                        ;;
+                    [nN])
+                        break
+                        ;;
+                    *)
+                        echo "Please enter y or n."
+                        ;;
+                esac
+            done
             ;;
         [nN])
-            echo "Skipping pyenv installation..."
-            echo "Done."
             break
             ;;
         *)
@@ -43,14 +94,8 @@ do
     esac
 done
 
-# Install Python
-echo "Installing Python $DEFAULT_PYTHON_VERSION and setting it as local default Python version..."
-pyenv install $DEFAULT_PYTHON_VERSION
-pyenv local $DEFAULT_PYTHON_VERSION
-echo "Done."
-
-# Install pip
-curl https://bootstrap.pypa.io/get-pip.py | python
+# Install pip for this pyenv version
+curl https://bootstrap.pypa.io/get-pip.py | pyenv exec python
 
 # Print help info
 echo
