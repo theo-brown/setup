972ee9575cd560e9a5b2ffda962d2a878fb4a2f0
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-18 16:43:35 +0100

    Update setup-python.sh

--- setup-python.sh
+++ setup-python.sh
@@ -1,23 +1,64 @@
+DEFAULT_PYTHON_VERSION=3.9.6
+
+START_DIR=$(pwd)
+cd $HOME
+
 # Install Python build dependencies
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 
-# Install pyenv
-curl https://pyenv.run | bash
+# Optionally remove existing pyenv
+while true
+do
+    read -rp "Existing pyenv found at $HOME/.pyenv, overwrite it? [y/n]: " input
+    case $input in
+        [yY])
+            echo "Removing $HOME/.pyenv..."  
+            rm -rf $HOME/.pyenv
+            echo "Done."
+       
+            # Install pyenv
+            echo "Installing pyenv to $HOME/.pyenv..."
+            
+            curl https://pyenv.run | bash
 
-echo "\n# Pyenv setup, generated automatically by Theo's setup-python.sh on $(date)" >> $HOME/.bashrc
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
-echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+            echo 
+            echo "# Pyenv setup, generated automatically by Theo's setup-python.sh on $(date)" >> $HOME/.bashrc
+            echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
+            echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+            echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+            
+            echo "Done."
+            break
+            ;;
+        [nN])
+            echo "Skipping pyenv installation..."
+            echo "Done."
+            break
+            ;;
+        *)
+            echo "Please enter y or n."
+            ;;
+    esac
+done
 
-# Update pip
-python -m ensurepip --upgrade
+# Install Python
+echo "Installing Python $DEFAULT_PYTHON_VERSION and setting it as local default Python version..."
+pyenv install $DEFAULT_PYTHON_VERSION
+pyenv local $DEFAULT_PYTHON_VERSION
+echo "Done."
+
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py | python
 
 # Print help info
+echo
 echo "To list installable Python versions, run:"
 echo " pyenv install -l"
 echo "To install a Python version run:"
 echo " pyenv install <version>"
 echo "To create a venv using a pyenv-installed Python version, run:"
 echo " pyenv virtualenv <version> <venv-name>"
+
+cd $START_DIR
